6ca4764695ef79c6a7a8194b78eeded97a107d7f
tanvishinde03 <shindetanvi0203@gmail.com>
2023-01-03 16:40:03 +0530

    checkevenodd

---
+++ evenodd.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace evenodd
+{
+    public  class evenoddnumber
+    {
+        public static void checkevenodd()
+        {
+
+            int Number;
+            Console.Write("Enter The Number: ");
+            Number = int.Parse(Console.ReadLine());
+            if (Number % 2 == 0)
+            {
+                Console.Write("Entered Number is an Even Number");
+            }
+            else
+            {
+                Console.Write("Entered Number is an Odd Number");
+            }
+            Console.Read();
+            Console.Write("----------------------------------------------");
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+    
+
