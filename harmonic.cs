26b150f60618d2924425c4113b02d478a78ead05
tanvishinde03 <shindetanvi0203@gmail.com>
2023-01-03 16:31:55 +0530

    checkharmonic

---
+++ harmonic.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basic_core_programs
+{
+    public class harmonic
+    {
+        public static void checkharmonic()
+        {
+
+            {
+                int i, n;
+                double s = 0.0;
+
+              
+                Console.Write("Calculate the harmonic series and their sum:");
+                Console.Write("----------------------------------------------");
+                
+
+                Console.Write("Input the number of terms : ");
+                n = Convert.ToInt32(Console.ReadLine());
+                
+                for (i = 1; i <= n; i++)
+                {
+                    Console.Write("1/{0} + ", i);
+                    s += 1 / (float)i;
+                }
+                Console.Write("Sum of Series upto {0} terms : {1} ", n, s);
+                Console.Write("----------------------------------------------");
+            }
+        }
+
+    }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
