c6823cccd632ac024e0ecf8bc0e9ad4a48b0b419
tanvishinde03 <shindetanvi0203@gmail.com>
2023-01-03 16:34:28 +0530

    checkfactors

---
+++ factors.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factorsceck
+{
+    public  class factors
+    {
+        public static void checkfactors()
+        {
+            int n,i;
+            Console.WriteLine("Enter Number to find the prime factors");
+
+            n = 0;
+            Console.ReadLine();
+
+            Console.WriteLine("factors");
+            for ( i = 2; i <= n; i++)
+            {
+                while (n % i == 0)
+                {
+                    Console.WriteLine("The prime factor of the number {0} is: {1}", n, i);
+                    n = n/ i;
+                }
+                Console.ReadLine();
+                Console.Write("----------------------------------------------");
+            }
+        }
+    }
+
+
+}
+
+
+    
+
