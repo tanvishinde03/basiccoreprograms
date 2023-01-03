aadfaba2296d28db494592daf6a165105744e5c4
tanvishinde03 <shindetanvi0203@gmail.com>
2023-01-03 16:38:15 +0530

    checkswap

---
+++ swap.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace swapnumberprogram
+{
+    public class swap
+    {
+        public static void swapnumber()
+        {
+
+            int num1, num2, temp;
+
+            Console.WriteLine("Enter first number");
+            num1 = 100;
+            Console.ReadLine();
+
+            Console.WriteLine("Enter second number");
+
+            num2 = 200; 
+            Console.ReadLine();
+
+            Console.WriteLine("Values before swapping: num1=" + num1 + " and num2=" + num2);
+
+            temp = num1;
+
+            num1 = num2;
+
+            num2 = temp;
+
+            Console.WriteLine("Values after swapping: num1=" + num1 + " and num2=" + num2);
+
+            
+
+            Console.ReadLine();
+
+            Console.Write("----------------------------------------------");
+
+
+
+
+
+
+        }
+
+    }
+}
