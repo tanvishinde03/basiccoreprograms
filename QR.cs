12f058a38e0dc3bd19fc7cfb3c5bcdb3fc5ecb56
tanvishinde03 <shindetanvi0203@gmail.com>
2023-01-03 16:36:38 +0530

    checkQR

---
+++ QR.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quotientandremainder
+{
+    public class QR
+    {
+       public static void checkQR()
+        {
+
+            int dividend = 50, divisor = 8;
+
+            int quotient = dividend / divisor;
+            int remainder = dividend % divisor;
+
+            Console.WriteLine("Dividend:{0} Divisor:{1}", dividend, divisor);
+            Console.WriteLine("Quotient = " + quotient);
+            Console.WriteLine("Remainder = " + remainder);
+            Console.ReadLine();
+            Console.Write("----------------------------------------------");
+        }
+    }
+
+
+
+
+
+
+
+}
+
