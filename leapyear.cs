1dfbedbdc7cdb8393ae920e697c4ea107348b820
tanvishinde03 <shindetanvi0203@gmail.com>
2023-01-03 16:22:31 +0530

    leapyear

---
+++ leapyear.cs
@@ -0,0 +1,28 @@
+﻿
+
+namespace yearcheck
+{
+    public class leapyear
+    {
+        public static void checkleapyear()
+        {
+            int chk_year;
+            Console.Write("Check whether a given year is leap year or not:");
+           
+
+            Console.Write("Input an year : ");
+            chk_year = Convert.ToInt32(Console.ReadLine());
+
+            if ((chk_year % 400) == 0)
+                Console.WriteLine("{0} is a leap year.", chk_year);
+            else if ((chk_year % 100) == 0)
+                Console.WriteLine("{0} is not a leap year.", chk_year);
+            else if ((chk_year % 4) == 0)
+                Console.WriteLine("{0} is a leap year.", chk_year);
+            else
+                Console.WriteLine("{0} is not a leap year.", chk_year);
+
+            Console.Write("----------------------------------------------");
+        }
+    }
+}
