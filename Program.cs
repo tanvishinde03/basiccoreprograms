7c939198982c5753f24f02d0f25fe18e18fbf9cf
tanvishinde03 <shindetanvi0203@gmail.com>
2023-01-03 16:48:49 +0530

    program

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using basicprograms;
+using basiccoreprograms;
+using yearcheck;
+using thepowerof2;
+using basic_core_programs;
+using factorsceck;
+using quotientandremainder;
+using swapnumberprogram;
+using evenodd;
+using vowelorconstant;
+using largestnumber;
+
+
+
+
+namespace basiccoreprograms
+{
+public class program
+    {
+public static void Main(String[] args)
+        {
+            flipcoin.FliptheCoin();
+            leapyear.checkleapyear();
+            powerof2.checkpowerof2();
+            harmonic.checkharmonic();
+            factors.checkfactors();
+            QR.checkQR();
+            swap.swapnumber();
+            evenoddnumber.checkevenodd();
+            alphabet.tocheckvc();
+            largetest.checknumber();
+        }
+
+
+
+    }
+
+
+
+    
+}
+
